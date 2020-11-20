05e71b98f428a2f691c76cca286d08ce3b824399
Christian Kinley <ckinley360@users.noreply.github.com>
2020-11-19 16:44:46 -0800

    Create Main.java

---
+++ java/different kinds of boxes - abstract class/Main.java
@@ -0,0 +1,35 @@
+
+public class Main {
+ 
+    public static void main(String[] args) {
+        // Test for the BoxWithMaxWeight class
+        BoxWithMaxWeight coffeeBox = new BoxWithMaxWeight(10);
+        coffeeBox.add(new Item("Saludo", 5));
+        coffeeBox.add(new Item("Pirkka", 5));
+        coffeeBox.add(new Item("Kopi Luwak", 5));
+ 
+        System.out.println(coffeeBox.isInBox(new Item("Saludo")));
+        System.out.println(coffeeBox.isInBox(new Item("Pirkka")));
+        System.out.println(coffeeBox.isInBox(new Item("Kopi Luwak")));
+        
+        System.out.println("");
+        
+        // Test for the OneItemBox class
+        OneItemBox box = new OneItemBox();
+        box.add(new Item("Saludo", 5));
+        box.add(new Item("Pirkka", 5));
+ 
+        System.out.println(box.isInBox(new Item("Saludo")));
+        System.out.println(box.isInBox(new Item("Pirkka")));
+        
+        System.out.println("");
+        
+        // Test for the MisplacingBox class
+        MisplacingBox misplacingBox = new MisplacingBox();
+        misplacingBox.add(new Item("Saludo", 5));
+        misplacingBox.add(new Item("Pirkka", 5));
+ 
+        System.out.println(misplacingBox.isInBox(new Item("Saludo")));
+        System.out.println(misplacingBox.isInBox(new Item("Pirkka")));
+    }
+}
