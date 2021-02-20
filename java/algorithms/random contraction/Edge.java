200301c46d9454173c2001ebce0ec4e103f82471
Christian Kinley <ckinley360@users.noreply.github.com>
2021-02-20 12:18:15 -0800

    Update Edge.java

--- java/algorithms/random contraction/Edge.java
+++ java/algorithms/random contraction/Edge.java
@@ -16,4 +16,44 @@ public class Edge {
     public Vertex getEndpointTwo() {
         return this.endpointTwo;
     }
+    
+    public void setEndpointOne(Vertex vertex) {
+        this.endpointOne = vertex;
+    }
+    
+    public void setEndpointTwo(Vertex vertex) {
+        this.endpointTwo = vertex;
+    }
+    
+    public boolean isSelfLoop() {
+        return this.endpointOne.equals(this.endpointTwo);
+    }
+    
+    @Override
+    public boolean equals(Object obj) {
+        // Compare the references.
+        if (this == obj) {
+            return true;
+        }
+        
+        // Ensure the object is not null.
+        if (obj == null) {
+            return false;
+        }
+        
+        // Ensure the object is of Edge class.
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        
+        // Convert the object to Edge type, and compare the names.
+        Edge other = (Edge) obj;
+        
+        return this.endpointOne.equals(other.getEndpointOne()) && this.endpointTwo.equals(other.getEndpointTwo());
+    }
+    
+    @Override
+    public String toString() {
+        return ("Endpoint One: " + this.endpointOne.getName() + ", Endpoint Two: " + this.endpointTwo.getName());
+    }
 }
