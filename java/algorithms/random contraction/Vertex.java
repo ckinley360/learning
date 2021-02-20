b59131a0251f267d8fc1ea45f8f85e0c8a8ea1d2
Christian Kinley <ckinley360@users.noreply.github.com>
2021-02-20 12:19:20 -0800

    Update Vertex.java

--- java/algorithms/random contraction/Vertex.java
+++ java/algorithms/random contraction/Vertex.java
@@ -24,6 +24,28 @@ public class Vertex {
         this.edges.add(edge);
     }
     
+    public void removeEdge(Edge edge) {
+        this.edges.remove(edge);
+    }
+    
+    public void transferEdges(Vertex transferToVertex) {
+        for (Edge edge:this.edges) {
+            
+            // If needed, add current edge to the transferToVertex.
+            if (edge.getEndpointOne().equals(this) || edge.getEndpointTwo().equals(this)) {
+                transferToVertex.addEdge(edge);
+            }
+            
+            if (edge.getEndpointOne().equals(this)) {
+                edge.setEndpointOne(transferToVertex);
+            }
+            
+            if (edge.getEndpointTwo().equals(this)) {
+                edge.setEndpointTwo(transferToVertex);
+            }
+        }
+    }
+    
     @Override
     public boolean equals(Object obj) {
         // Compare the references.
@@ -62,6 +84,9 @@ public class Vertex {
                 output.append(edge.getEndpointTwo().getName() + ", ");
             }
         }
+        
+        // Delete the extra comma and space at the end of the string.
+        output.delete(output.length() - 2, output.length());
         
         output.append("]");
         
