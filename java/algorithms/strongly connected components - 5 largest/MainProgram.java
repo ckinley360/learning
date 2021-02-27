dc07d12848dcb0a68b3926f183b53c3240834c80
Christian Kinley <ckinley360@users.noreply.github.com>
2021-02-27 10:46:43 -0800

    Update MainProgram.java

--- java/algorithms/strongly connected components - 5 largest/MainProgram.java
+++ java/algorithms/strongly connected components - 5 largest/MainProgram.java
@@ -7,16 +7,66 @@ import java.util.ArrayList;
 public class MainProgram {
     
     public static int finishingTime;
+    public static Vertex leader;
 
     public static void main(String[] args) {
         // Create the graph from the data file.
         DirectedGraph graph = readDataFromFile("data.txt");
+        
+        // Run Kosaraju's two-pass algorithm.
         depthFirstSearchLoopReverse(graph);
-        System.out.println(graph);
+        depthFirstSearchLoop(graph);
+        
+        // Calculate and display the top five SCC sizes.
+        ArrayList<Integer> topFiveSccSizes = graph.getFiveLargestSccs();
+        for (int size : topFiveSccSizes) {
+            System.out.println(size);
+        }
+    }
+    
+     
+    
+    public static void depthFirstSearchLoop(DirectedGraph graph) {
+        // Initialize leader to null.
+        leader = null;
+        
+        // Store the vertices list address in vertices variable.
+        ArrayList<Vertex> vertices = graph.getVertices();
+        
+        // Sort the vertices descending.
+        Collections.sort(vertices, Collections.reverseOrder());
+        
+        // Call depth first search on every vertex in the graph. Reset finishing time to 0, which will be used to store the size of each SCC.
+        for (int i = 0; i < vertices.size(); i++) {
+            finishingTime = 0;
+            if (!vertices.get(i).isExplored()) {
+                leader = vertices.get(i);
+                depthFirstSearch(graph, vertices.get(i));
+            }
+        }
+    }
+    
+    public static void depthFirstSearch(DirectedGraph graph, Vertex startNode) {
+        // Mark start node as explored.
+        startNode.setExplored(true);
+        
+        // Set the leader of start node.
+        startNode.setLeader(leader);
+        
+        // Explore all of start node's accessible neighbor nodes.
+        for (Edge edgeTail : startNode.getEdgeTails()) {
+            if (!edgeTail.getHead().isExplored()) {
+                depthFirstSearch(graph, edgeTail.getHead());
+            }
+        }
+        
+        // Set the SCC size of the start node.
+        finishingTime++;
+        startNode.setSccSize(finishingTime);
     }
     
     public static void depthFirstSearchLoopReverse(DirectedGraph graph) {
-        // Create variable to set finishing time of each vertex.
+        // Initialize finishing time to 0.
         finishingTime = 0;
         
         // Store the vertices list address in vertices variable.
@@ -25,11 +75,18 @@ public class MainProgram {
         // Sort the vertices descending.
         Collections.sort(vertices, Collections.reverseOrder());
         
+        // Call depth first search reverse on every vertex in the graph.
         for (int i = 0; i < vertices.size(); i++) {
             if (!vertices.get(i).isExplored()) {
-                depthFirstSearchReverse(graph, i);
+                depthFirstSearchReverse(graph, vertices.get(i));
             }
         }
+        
+        // Set all vertices back to unexplored.
+        graph.setVerticesExplored(false);
+        
+        // Set all vertices' names to their finishing time.
+        graph.setVerticesNamesToFinishingTime();
     }
     
     public static void depthFirstSearchReverse(DirectedGraph graph, Vertex startNode) {
