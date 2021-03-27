1ae6a6183954942a0e3e3e110e5e403d71d485a4
Christian Kinley <ckinley360@users.noreply.github.com>
2021-03-27 14:34:36 -0700

    Update MainProgram.java

--- java/algorithms/scheduling jobs/MainProgram.java
+++ java/algorithms/scheduling jobs/MainProgram.java
@@ -6,15 +6,45 @@ import java.util.Arrays;
 public class MainProgram {
     
     public static void main(String[] args) {
+        // Create the Job arrays.
+        Job[] jobs = readDataFromFile("testdata.txt", "difference");
+        Job[] jobs2 = readDataFromFile("testdata.txt", "quotient");
         
+        // Sort the Job arrays based on score.
+        Arrays.sort(jobs);
+        Arrays.sort(jobs2);
+        
+        // Compute the completion time of each job.
+        int sumOfWeightedCompletionTimes = computeSumOfWeightedCompletionTimes(jobs);
+        int sumOfWeightedCompletionTimes2 = computeSumOfWeightedCompletionTimes(jobs2);
+        
+        // Print the sum of weighted completion times.
+        System.out.println("Sum of weighted completion times (difference): " + sumOfWeightedCompletionTimes);
+        System.out.println("Sum of weighted completion times (quotient): " + sumOfWeightedCompletionTimes2);
     }
     
-    public static Job[] readDataFromFile(String filePath) {
+    public static int computeSumOfWeightedCompletionTimes(Job[] jobs) {
+        // Variable to track the running total of completion time.
+        int completionTime = 0;
+        double sumOfWeightedCompletionTimes = 0;
+        
+        for (Job job : jobs) {
+            completionTime += job.getLength();
+            sumOfWeightedCompletionTimes += (double) job.getWeight() * completionTime;
+        }
+        
+        return (int) sumOfWeightedCompletionTimes;
+    }
+    
+    public static Job[] readDataFromFile(String filePath, String scoreType) {
         // Create the Job array to store the jobs in.
-        Job[] jobs = new Job[10000];
+        Job[] jobs = new Job[40];
         
         // Create index tracker for the Job array.
         int i = 0;
+        
+        // Boolean to track whether we are reading the first row, which we want to ignore because it only tells us the total number of jobs.
+        Boolean firstRow = true;
         
         // Create a Scanner object for reading the file.
         try (Scanner scanner = new Scanner(Paths.get(filePath))) {
@@ -24,6 +54,12 @@ public class MainProgram {
                 // Read one line.
                 String row = scanner.nextLine();
                 
+                // Extract the data from the row only if it is not the first row.
+                if (firstRow) {
+                    firstRow = false;
+                    continue;
+                }
+                
                 // Split the line on space character.
                 String[] parts = row.split(" ");
                 
@@ -32,7 +68,7 @@ public class MainProgram {
                 int length = Integer.valueOf(parts[1]);
                 
                 // Create a Job object from the row.
-                Job job = new Job(weight, length, "difference");
+                Job job = new Job(weight, length, scoreType);
                 
                 // Add the Job object to the array.
                 jobs[i] = job;
