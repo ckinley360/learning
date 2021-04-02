21110d1bac8d7e1d852584bab159e9238058aefd
Christian Kinley <ckinley360@users.noreply.github.com>
2021-04-01 20:04:51 -0700

    Create UnionFind.java

---
+++ java/algorithms/max spacing k clustering - small graph/UnionFind.java
@@ -0,0 +1,15 @@
+
+import java.util.Map;
+import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
+
+public class UnionFind {
+    
+    private Map<Integer, Integer> leaders;
+    private Map<Integer, List<Integer>> followers;
+    
+    public UnionFind(Graph graph) {
+        for (Vertex vertex : graph.getVertices().getV)
+    }
+}
