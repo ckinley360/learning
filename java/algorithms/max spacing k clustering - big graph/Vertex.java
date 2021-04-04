32c28acedca9051bdeca072925d41529941e839b
Christian Kinley <ckinley360@users.noreply.github.com>
2021-04-03 20:07:47 -0700

    Create Vertex.java

---
+++ java/algorithms/max spacing k clustering - big graph/Vertex.java
@@ -0,0 +1,83 @@
+
+import java.util.ArrayList;
+import java.lang.StringBuilder;
+
+public class Vertex {
+
+    private int name;
+    private ArrayList<Edge> edges;
+    private long bitLabel;
+    
+    public Vertex(int name, String binaryString) {
+        this.name = name;
+        this.edges = new ArrayList<>();
+        this.bitLabel = Long.parseLong(binaryString, 2);
+    }
+    
+    public int getName() {
+        return this.name;
+    }
+    
+    public long getBitLabel() {
+        return this.bitLabel;
+    }
+    
+    public ArrayList<Edge> getEdges() {
+        return this.edges;
+    }
+    
+    public void addEdge(Edge edge) {
+        this.edges.add(edge);
+    }
+    
+    public void removeEdge(Edge edge) {
+        this.edges.remove(edge);
+    }
+    
+    @Override
+    public boolean equals(Object obj) {
+        // Compare the references.
+        if (this == obj) {
+            return true;
+        }
+        
+        // Ensure the object is not null.
+        if (obj == null) {
+            return false;
+        }
+        
+        // Ensure the object is of Vertex class.
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        
+        // Convert the object to Vertex type, and compare the names.
+        Vertex other = (Vertex) obj;
+        
+        return this.name == other.getName();
+    }
+    
+    @Override
+    public String toString() {
+        StringBuilder output = new StringBuilder();
+        
+        output.append(this.name + ": [");
+        
+        for (Edge edge:this.edges) {
+            if (!edge.getEndpointOne().equals(this)) {
+                output.append(edge.getEndpointOne().getName() + "|" + edge.getLength() + ", ");
+            }
+            
+            if (!edge.getEndpointTwo().equals(this)) {
+                output.append(edge.getEndpointTwo().getName() + "|" + edge.getLength() + ", ");
+            }
+        }
+        
+        // Delete the extra comma and space at the end of the string.
+        output.delete(output.length() - 2, output.length());
+        
+        output.append("]");
+        
+        return output.toString();
+    }
+}
