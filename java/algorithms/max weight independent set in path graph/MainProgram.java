c0f5d6e9400e4500e3164858f3a07592dafdff82
Christian Kinley <ckinley360@users.noreply.github.com>
2021-04-18 07:58:28 -0700

    Update MainProgram.java

--- java/algorithms/max weight independent set in path graph/MainProgram.java
+++ java/algorithms/max weight independent set in path graph/MainProgram.java
@@ -6,30 +6,42 @@ public class MainProgram {
     
     public static void main(String[] args) {
         // Create the array from the data file.
-        int[] data = createArrayFromFile("data.txt");
+        int[] data = createArrayFromFile("testdata.txt");
         
-        for (int weight : data) {
-            System.out.println(weight);
+        // Compute the values of the max weight independent set.
+        int[] values = computeMaxWeightIndependentSetValues(data);
+        
+        for (int value : values) {
+            System.out.println(value);
         }
     }
     
     public static int[] computeMaxWeightIndependentSetValues(int[] data) {
         // Create the array to store the max weight independent set values in.
-        int[] values = new int[1001];
+        int[] values = new int[7];
         
         // Insert the initial values for the empty set and single node set.
         values[0] = 0;
         values[1] = data[0];
         
         // Compute the remaining values.
-        for (int i = 1; i < 999; i++) {
+        for (int i = 2; i < 7; i++) {
+            int lastExcluded = values[i - 1];
+            int secondToLastExcluded = values[i - 2] + data[i - 1];
             
+            if (lastExcluded > secondToLastExcluded) {
+                values[i] = lastExcluded;
+            } else {
+                values[i] = secondToLastExcluded;
+            }
         }
+        
+        return values;
     }
     
     public static int[] createArrayFromFile(String filePath) {
         // Create the array to store the data in.
-        int[] data = new int[1000];
+        int[] data = new int[6];
         
         // Boolean to track whether we are reading the first row, which we want to ignore because it only tells us the total number of nodes in the path graph.
         Boolean firstRow = true;
