2bb288e6b2bd5ed8567c384707a30bc3b22d1aa5
Christian Kinley <ckinley360@users.noreply.github.com>
2021-04-18 09:18:23 -0700

    Update MainProgram.java

--- java/algorithms/max weight independent set in path graph/MainProgram.java
+++ java/algorithms/max weight independent set in path graph/MainProgram.java
@@ -2,12 +2,13 @@
 import java.nio.file.Paths;
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.lang.StringBuilder;
 
 public class MainProgram {
     
     public static void main(String[] args) {
         // Create the array from the data file.
-        int[] data = createArrayFromFile("testdata.txt");
+        int[] data = createArrayFromFile("data.txt");
         
         // Compute the values of the max weight independent set.
         int[] values = computeMaxWeightIndependentSetValues(data);
@@ -15,9 +16,59 @@ public class MainProgram {
         // Compute the nodes of the max weight independent set.
         ArrayList<Integer> nodes = computeMaxWeightIndependentSetNodes(data, values);
         
-        for (int node : nodes) {
-            System.out.println(node);
+        // Construct the output string.
+        StringBuilder outputString = new StringBuilder();
+        
+        if (nodes.contains(1)) {
+            outputString.append(1);
+        } else {
+            outputString.append(0);
         }
+        
+        if (nodes.contains(2)) {
+            outputString.append(1);
+        } else {
+            outputString.append(0);
+        }
+        
+        if (nodes.contains(3)) {
+            outputString.append(1);
+        } else {
+            outputString.append(0);
+        }
+        
+        if (nodes.contains(4)) {
+            outputString.append(1);
+        } else {
+            outputString.append(0);
+        }
+        
+        if (nodes.contains(17)) {
+            outputString.append(1);
+        } else {
+            outputString.append(0);
+        }
+        
+        if (nodes.contains(117)) {
+            outputString.append(1);
+        } else {
+            outputString.append(0);
+        }
+        
+        if (nodes.contains(517)) {
+            outputString.append(1);
+        } else {
+            outputString.append(0);
+        }
+        
+        if (nodes.contains(997)) {
+            outputString.append(1);
+        } else {
+            outputString.append(0);
+        }
+        
+        // Print the output string.
+        System.out.println(outputString);
     }
     
     public static ArrayList<Integer> computeMaxWeightIndependentSetNodes(int[] data, int[] values) {
@@ -26,12 +77,18 @@ public class MainProgram {
         
         for (int i = values.length - 1; i >= 1; i--) {
             int lastExcluded = values[i - 1];
-            int secondToLastExcluded = values[i - 2] + data[i - 1];
+            int secondToLastExcluded;
+            
+            if (i == 1) {
+                secondToLastExcluded = data[i - 1];
+            } else {
+                secondToLastExcluded = values[i - 2] + data[i - 1];
+            }
             
             if (lastExcluded >= secondToLastExcluded) {
                 continue;
             } else {
-                nodes.add(data[i - 1]);
+                nodes.add(i);
                 i--;
             }
         }
@@ -41,14 +98,14 @@ public class MainProgram {
     
     public static int[] computeMaxWeightIndependentSetValues(int[] data) {
         // Create the array to store the max weight independent set values in.
-        int[] values = new int[7];
+        int[] values = new int[1001];
         
         // Insert the initial values for the empty set and single node set.
         values[0] = 0;
         values[1] = data[0];
         
         // Compute the remaining values.
-        for (int i = 2; i < 7; i++) {
+        for (int i = 2; i < 1001; i++) {
             int lastExcluded = values[i - 1];
             int secondToLastExcluded = values[i - 2] + data[i - 1];
             
@@ -64,7 +121,7 @@ public class MainProgram {
     
     public static int[] createArrayFromFile(String filePath) {
         // Create the array to store the data in.
-        int[] data = new int[6];
+        int[] data = new int[1000];
         
         // Boolean to track whether we are reading the first row, which we want to ignore because it only tells us the total number of nodes in the path graph.
         Boolean firstRow = true;
