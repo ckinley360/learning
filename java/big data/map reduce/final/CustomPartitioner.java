25104e48c3b73db29d6768b899a85ed93055544d
Christian Kinley <ckinley360@users.noreply.github.com>
2021-07-21 16:36:08 -0700

    Create CustomPartitioner.java

---
+++ java/big data/map reduce/final/CustomPartitioner.java
@@ -0,0 +1,43 @@
+package stubs;
+
+import org.apache.avro.mapred.AvroKey;
+import org.apache.avro.mapred.AvroValue;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.mapreduce.Partitioner;
+
+public class CustomPartitioner extends Partitioner<AvroKey<Suit>, AvroValue<Card>> {
+
+	@Override
+	public int getPartition(AvroKey<Suit> key, AvroValue<Card> value, int numPartitions) {
+		String suit = key.datum().toString().toLowerCase();
+		
+		// Each suit is evenly distributed, so try to evenly distribute each suit to its own partition.
+		if (numPartitions == 0 || numPartitions == 1) {
+			return 0;
+		} else if (numPartitions == 2) { 
+			if (suit.equals("club") || suit.equals("spade")) {
+				return 0;
+			} else {
+				return 1;
+			}
+		} else if (numPartitions == 3) {
+			if (suit.equals("club") || suit.equals("spade")) {
+				return 0;
+			} else if (suit.equals("diamond")) {
+				return 1;
+			} else {
+				return 2;
+			}
+		} else {
+			if (suit.equals("club")) {
+				return 0;
+			} else if (suit.equals("spade")) {
+				return 1;
+			} else if (suit.equals("diamond")) {
+				return 2;
+			} else {
+				return 3;
+			}
+		}
+	}
+}
