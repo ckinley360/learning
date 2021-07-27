e6f907628953c1e3c3f8ff3a80d442726abd5fc2
Christian Kinley <ckinley360@users.noreply.github.com>
2021-07-27 16:24:32 -0700

    Add count job

--- java/big data/map reduce/parquet avro mapreduce/ParquetAvroDriver.java
+++ java/big data/map reduce/parquet avro mapreduce/ParquetAvroDriver.java
@@ -1,12 +1,17 @@
 package stubs;
 
+import org.apache.avro.mapreduce.AvroJob;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
+import org.apache.parquet.avro.AvroParquetInputFormat;
 import org.apache.parquet.avro.AvroParquetOutputFormat;
 
 public class ParquetAvroDriver extends Configured implements Tool {
@@ -50,6 +55,42 @@ public class ParquetAvroDriver extends Configured implements Tool {
 		
 		// Submit the job and wait for completion
 		boolean success = etlJob.waitForCompletion(true);
+		
+		if (success == false) {
+			System.err.println("First phase failed");
+			return 1;
+		}
+		
+		/* Job 2: The Suit Counting Job */
+		Job countJob = Job.getInstance();
+		countJob.setJarByClass(ParquetAvroDriver.class);
+		countJob.setJobName("Count Job");
+		
+		// Set AvroParquet Input Path and File Output Path
+		AvroParquetInputFormat.addInputPath(countJob, new Path(output + "-etl"));
+		FileOutputFormat.setOutputPath(countJob, new Path(output));
+		
+		// Set the input as Parqet and output as regular text
+		countJob.setInputFormatClass(AvroParquetInputFormat.class);
+		countJob.setOutputFormatClass(TextOutputFormat.class);
+		
+		// Set the Mapper and Reducer Classes
+		countJob.setMapperClass(CardMapper.class);
+		countJob.setReducerClass(CardTotalReducer.class);
+		
+		// Set up the Avro schemas for the Mapper so they are read correctly by the Reducer
+		AvroJob.setMapOutputKeySchema(countJob, Suit.getClassSchema());
+		AvroJob.setMapOutputValueSchema(countJob, Card.getClassSchema());
+		
+		// Set the job output key and value classes
+		countJob.setOutputKeyClass(Text.class);
+		countJob.setOutputValueClass(Integer.class);
+		
+		// Create the projection for the cards stored in the file
+		AvroParquetInputFormat.setRequestedProjection(countJob, Card.getClassSchema());
+		
+		// Submit the job and wait for completion
+		success = countJob.waitForCompletion(true);
 		
 		return success ? 0 : 1;
 	}
