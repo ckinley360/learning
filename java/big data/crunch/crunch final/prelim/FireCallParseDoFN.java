d3f9cb52e9e12a3f337e72542fe509adc3fdb7a2
Christian Kinley <ckinley360@users.noreply.github.com>
2021-09-02 16:18:01 -0700

    Update FireCallParseDoFN.java

--- java/big data/crunch/crunch final/prelim/FireCallParseDoFN.java
+++ java/big data/crunch/crunch final/prelim/FireCallParseDoFN.java
@@ -1,5 +1,94 @@
 package prelim;
 
-public class FireCallParseDoFN {
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.crunch.DoFn;
+import org.apache.crunch.Emitter;
+
+import model.FireCall;
+
+public class FireCallParseDoFN extends DoFn<String, FireCall> {
+
+	private static final long serialVersionUID = 1L;
+	
+	/*
+	 * Example data:
+	 * "ALARM_LEVEL","CALL_TYPE","JURISDICTION","STATION","RECEIVED_DATE","RECEIVED_TIME","DISPATCH_1ST_TIME","ONSCENE_1ST_TIME","FIRE_CONTROL_TIME","CLOSE_TIME"
+	 * "1","UNK/ EMS","RF","UNK","12/31/2011","23:54:40","","","","00:03:54"
+	 */
+
+	private static Pattern inputPattern;
+	
+	private static final Log LOG = LogFactory.getLog(FireCallParseDoFN.class);
+	
+	private SimpleDateFormat yearFormat;
+	private SimpleDateFormat timeFormat;
+	
+	@Override
+	public void initialize() {
+		inputPattern = Pattern.compile("^(.*),(.*),(.*),(.*),(.*),(.*),(.*),(.*),(.*),(.*)$");
+		
+		yearFormat = new SimpleDateFormat("MM/dd/yyyy");
+		
+		timeFormat = new SimpleDateFormat("HHmmss");
+	}
+	
+	@Override
+	public void process(String line, Emitter<FireCall> emitter) {
+		Matcher inputMatch = inputPattern.matcher(line);
+		
+		if (inputMatch.matches()) {
+			int priority = Integer.parseInt(inputMatch.group(1).replace("\"", ""));
+			String callType = inputMatch.group(2).replace("\"", "");
+			String jurisdiction = inputMatch.group(3).replace("\"", "");
+			String station = inputMatch.group(4).replace("\"", "");
+			Long receiveTime = null;
+			Long dispatchTime = null;
+			Long arrivalTime = null;
+			Long clearTime = null;
+			
+			// Assign a long to each time field, which is the datetime created by combining the time and callDate
+			try {
+				String callDateString = inputMatch.group(5).replace("\"", "");
+				Date callDate = yearFormat.parse(callDateString);
+				receiveTime = getFullTime(inputMatch.group(6).replace("\"", ""), callDate);
+				dispatchTime = getFullTime(inputMatch.group(7).replace("\"", ""), callDate);
+				arrivalTime = getFullTime(inputMatch.group(8).replace("\"", ""), callDate);
+				clearTime = getFullTime(inputMatch.group(10).replace("\"", ""), callDate);
+			} catch (ParseException e) {
+				LOG.error("Exception thrown while parsing date. Input was \"" + line + "\".", e);
+			}
+			
+			FireCall fireCall = new FireCall();
+			fireCall.setPriority(priority);
+			fireCall.setCallType(callType);
+			fireCall.setJurisdiction(jurisdiction);
+			fireCall.setStation(station);
+			fireCall.setReceiveTime(receiveTime);
+			fireCall.setDispatchTime(dispatchTime);
+			fireCall.setArrivalTime(arrivalTime);
+			fireCall.setClearTime(clearTime);
+			
+			emitter.emit(fireCall);
+		} else {
+			LOG.warn("Could not parse. Input was \"" + line + "\".");
+		}
+			
+	}
+	
+	// Produces a long representing the datetime created by combining the time and callDate
+	private long getFullTime(String time, Date callDate) throws ParseException {
+		Date callTime = timeFormat.parse(time);
+		
+		long totalTime = callTime.getTime();
+		totalTime += callDate.getTime();
+		
+		return totalTime;
+	}
 }
