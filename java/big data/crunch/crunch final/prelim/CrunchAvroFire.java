88383c10fc5f7956bc345906dc71c888f6ce87fd
Christian Kinley <ckinley360@users.noreply.github.com>
2021-09-02 16:24:08 -0700

    Update CrunchAvroFire.java

--- java/big data/crunch/crunch final/prelim/CrunchAvroFire.java
+++ java/big data/crunch/crunch final/prelim/CrunchAvroFire.java
@@ -2,12 +2,17 @@ package prelim;
 
 import org.apache.crunch.PCollection;
 import org.apache.crunch.Pipeline;
+import org.apache.crunch.PipelineResult;
 import org.apache.crunch.impl.mr.MRPipeline;
 import org.apache.crunch.io.From;
+import org.apache.crunch.io.To;
+import org.apache.crunch.types.avro.Avros;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
+
+import model.FireCall;
 
 public class CrunchAvroFire extends Configured implements Tool {
 
@@ -33,6 +38,17 @@ public class CrunchAvroFire extends Configured implements Tool {
 		// Read the data from the text file
 		PCollection<String> fireCallLines = pipeline.read(From.textFile(fireCallInput));
 		
+		// Parse the data and convert to FireCall objects
+		PCollection<FireCall> fireCalls = fireCallLines.parallelDo(
+				new FireCallParseDoFN(), 
+				Avros.specifics(FireCall.class));
 		
+		// Write the data to an Avro file
+		fireCalls.write(To.avroFile(output));
+		
+		// Submit the job for execution
+		PipelineResult result = pipeline.done();
+		
+		return result.succeeded() ? 0 : 1;
 	}
 }
