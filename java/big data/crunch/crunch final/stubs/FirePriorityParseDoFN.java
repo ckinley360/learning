ff608df9e5d38fb8c9db72f11c65b315dc7ef45e
Christian Kinley <ckinley360@users.noreply.github.com>
2021-09-03 16:56:55 -0700

    Update FirePriorityParseDoFN.java

--- java/big data/crunch/crunch final/stubs/FirePriorityParseDoFN.java
+++ java/big data/crunch/crunch final/stubs/FirePriorityParseDoFN.java
@@ -1,5 +1,17 @@
 package stubs;
 
-public class FirePriorityParseDoFN {
+import org.apache.crunch.DoFn;
+import org.apache.crunch.Emitter;
+import org.apache.crunch.Pair;
 
+import model.FireCall;
+
+public class FirePriorityParseDoFN extends DoFn<FireCall, Pair<Integer, FireCall>> {
+
+	private static final long serialVersionUID = 1226893760849609277L;
+
+	@Override
+	public void process(FireCall call, Emitter<Pair<Integer, FireCall>> emitter) {
+		emitter.emit(new Pair<Integer, FireCall>(call.getPriority(), call));
+	}
 }
