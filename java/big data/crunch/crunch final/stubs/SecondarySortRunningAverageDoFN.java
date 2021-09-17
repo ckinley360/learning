7c90166d67859fba00011e0a9b32280633d118c8
Christian Kinley <ckinley360@users.noreply.github.com>
2021-09-17 16:11:58 -0700

    Update SecondarySortRunningAverageDoFN.java

--- java/big data/crunch/crunch final/stubs/SecondarySortRunningAverageDoFN.java
+++ java/big data/crunch/crunch final/stubs/SecondarySortRunningAverageDoFN.java
@@ -1,13 +1,20 @@
 package stubs;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
 import org.apache.crunch.DoFn;
 import org.apache.crunch.Emitter;
 import org.apache.crunch.Pair;
+import org.apache.hadoop.conf.Configuration;
 
 public class SecondarySortRunningAverageDoFN extends DoFn<Pair<String, Iterable<Pair<String, Double>>>, Pair<String, Pair<String, Double>>> {
 
 	private static final long serialVersionUID = -6690441199918684556L;
-
+	private static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd", Locale.ENGLISH);
+	private static LocalDate runningAverageEndDate;
+	
 	@Override
 	public void process(Pair<String, Iterable<Pair<String, Double>>> input, Emitter<Pair<String, Pair<String, Double>>> emitter) {
 		String callType = input.first();
@@ -16,13 +23,24 @@ public class SecondarySortRunningAverageDoFN extends DoFn<Pair<String, Iterable<
 		
 		for (Pair<String, Double> dateAndCost : input.second()) {
 			String date = dateAndCost.first();
+			LocalDate convertedDate = LocalDate.parse(date, formatter);
 			Double cost = dateAndCost.second();
 			count++;
 			sum += cost;
 			double average = sum / count;
 			Pair<String, Double> dateAndCostRunningAverage = new Pair<String, Double>(date, average);
 			
-			emitter.emit(new Pair<String, Pair<String, Double>>(callType, dateAndCostRunningAverage));
+			// If the date is on or before the user-specified end date, then emit it.
+			if (convertedDate.isBefore(runningAverageEndDate) || convertedDate.isEqual(runningAverageEndDate)) {
+				emitter.emit(new Pair<String, Pair<String, Double>>(callType, dateAndCostRunningAverage));
+			}
 		}
+	}
+	
+	// https://hadoopsters.com/2015/11/11/apache-crunch-tutorial-5-hadoop-configurations/
+	// https://hadoopsters.com/2015/11/11/apache-crunch-tutorial-6-configurations-on-a-per-job-basis/
+	@Override
+	public void configure(Configuration conf) {
+		runningAverageEndDate = LocalDate.parse(conf.get("running_average_end_date"), formatter);
 	}
 }
