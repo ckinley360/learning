438ac70cbbd112129415e014718b8a2d1ffdad19
Christian Kinley <ckinley360@users.noreply.github.com>
2021-09-29 17:55:48 -0700

    Create timestampify.py

---
+++ java/big data/hive/transforms/timestampify.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+
+import sys
+from datetime import datetime
+import re
+
+# Sample input header and row:
+# Priority,Call_Type,Jurisdiction,Dispatch_Area,Received_Date,Received_Time,Dispatch_Time,Arrival_Time,Cleared_Time,Disposition
+# 3,SUSPV,RP,RS, 03/21/2013,173011,182946,182946,183107,OK
+# https://www.programiz.com/python-programming/online-compiler/
+
+# Iterate through every line passed in to stdin
+for line in sys.stdin.readlines():
+	# Split incoming pieces on commas
+	pieces = line.strip().split(",")
+
+	# Put each piece into its own variable. Make each variable a string, and remove leading and
+	# trailing whitespace to deal with bad data.
+	priority = str(pieces[0]).strip(),
+	callType = str(pieces[1]).strip(),
+	jurisdiction = str(pieces[2]).strip(),
+	dispatchArea = str(pieces[3]).strip(),
+	receivedDate = str(pieces[4]).strip(),
+	receivedTime = str(pieces[5]).strip(),
+	dispatchTime = str(pieces[6]).strip(),
+	arrivalTime = str(pieces[7]).strip(),
+	clearedTime = str(pieces[8]).strip(),
+	disposition = str(pieces[9]).strip()
+
+	# If the receivedDate has bad data, then skip this line of data.
+	try:
+		convertedReceivedDate = str(datetime.strptime(receivedDate, '%m/%d/%Y').date())
+	except:
+		continue
+
+	# If the times are good data, then merge them with the date.
+	pattern = '[0-9]{6}'
+
+	if bool(re.match(pattern, receivedTime))
+		convertedReceivedTime = datetime.strptime(convertedReceivedDate + ' ' + receivedTime, '%Y-%m-%d %H%M%S')
