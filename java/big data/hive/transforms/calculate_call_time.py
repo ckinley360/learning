3fc6cc7f234ff52b35a07a1494bec2227153c6ca
Christian Kinley <ckinley360@users.noreply.github.com>
2021-10-06 17:27:32 -0700

    Create calculate_call_time.py

---
+++ java/big data/hive/transforms/calculate_call_time.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python
+
+import sys
+from datetime import datetime, timedelta
+import re
+
+# Sample input header and row:
+# Priority,Call_Type,Jurisdiction,Dispatch_Area,Received_Date,Received_Time,Dispatch_Time,Arrival_Time,Cleared_Time,Disposition
+# 3,SUSPV,RP,RS, 03/21/2013,173011,182946,182946,183107,OK
+# https://www.programiz.com/python-programming/online-compiler/
+
+try:
+	# Iterate through every line passed in to stdin
+	for line in sys.stdin.readlines():
+		# Split incoming pieces on tab character
+		pieces = line.strip().split("\t")
+
+		# If the line has 10 column values, then proceed. Otherwise, skip it.
+		if (len(pieces) == 10):
+			# Put each piece into its own variable. Make each variable a string, and remove leading and
+			# trailing whitespace to deal with bad data.
+			priority = str(pieces[0]).strip()
+			callType = str(pieces[1]).strip()
+			jurisdiction = str(pieces[2]).strip()
+			dispatchArea = str(pieces[3]).strip()
+			receivedDate = str(pieces[4]).strip()
+			receivedTime = str(pieces[5]).strip()
+			dispatchTime = str(pieces[6]).strip()
+			arrivalTime = str(pieces[7]).strip()
+			clearedTime = str(pieces[8]).strip()
+			disposition = str(pieces[9]).strip()
+
+			# If the receivedDate has bad data, then skip this line of data.
+			try:
+				convertedReceivedDate = str(datetime.strptime(receivedDate, '%m/%d/%Y').date())
+			except:
+				continue
+
+			# If the times are good data, then merge them with the date.
+			pattern = '[0-9]{6}'
+
+			if re.match(pattern, receivedTime):
+				convertedReceivedTime = datetime.strptime(convertedReceivedDate + ' ' + receivedTime, '%Y-%m-%d %H%M%S')
+			else:
+				receivedTime = '000000'
+				convertedReceivedTime = datetime.strptime(convertedReceivedDate + ' ' + receivedTime, '%Y-%m-%d %H%M%S')
+
+			if re.match(pattern, clearedTime):
+				convertedClearedTime = datetime.strptime(convertedReceivedDate + ' ' + clearedTime, '%Y-%m-%d %H%M%S')
+			else:
+				clearedTime = '000000'
+				convertedClearedTime = datetime.strptime(convertedReceivedDate + ' ' + clearedTime, '%Y-%m-%d %H%M%S')
+
+			# Calculate the call time
+			timeDifference = convertedClearedTime - convertedReceivedTime
+			timeDifferenceInSeconds = timeDifference.total_seconds()	
+
+			# Output the call time
+			print(int(timeDifferenceInSeconds))
+		else:
+			continue
+except:
+	# In the case of an exception, write the stack trace to stdout so we can see it in Hive, in the results of the UDF call.
+	print(sys.exc_info())
