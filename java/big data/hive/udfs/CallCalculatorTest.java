276783924f697c38ef2d3492af60b7a4a06d3851
Christian Kinley <ckinley360@users.noreply.github.com>
2021-10-13 17:42:13 -0700

    Create CallCalculatorTest.java

---
+++ java/big data/hive/udfs/CallCalculatorTest.java
@@ -0,0 +1,25 @@
+package stubs;
+
+import static org.junit.Assert.assertEquals;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CallCalculatorTest {
+
+	CallCalculator callCalculator;
+	
+	@Before
+	public void setup() {
+		callCalculator = new CallCalculator();
+	}
+	
+	@Test
+	public void testTimeDifference() {
+		long output = callCalculator.evaluate(new Text(" 4/16/2021"), new IntWritable(053000), new IntWritable(131503)).get();
+		
+		assertEquals("Output does not match", 465, output);
+	}
+}
