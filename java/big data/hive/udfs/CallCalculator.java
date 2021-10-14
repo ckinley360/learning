7deb43a199432d371186a4e8a245ca6c8e83e757
Christian Kinley <ckinley360@users.noreply.github.com>
2021-10-13 17:20:54 -0700

    Update CallCalculator.java

--- java/big data/hive/udfs/CallCalculator.java
+++ java/big data/hive/udfs/CallCalculator.java
@@ -5,7 +5,12 @@ import org.apache.hadoop.hive.ql.exec.UDF;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
+
+import java.time.Duration;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Description(name = "time_diff_seconds",
              value = "_FUNC_(date, startTime, endTime) - Calculates the difference in seconds.",
@@ -14,7 +19,51 @@ import java.time.LocalDateTime;
 public class CallCalculator extends UDF {
 
 	public LongWritable evaluate(Text date, IntWritable startTime, IntWritable endTime) {
-		// Check that the input data is of good quality. If not, ignore it and return 0.
+		// If the date is bad data, then return 0. Otherwise, parse it.
+		String stringDate = date.toString().trim();
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("M/d/yyyy");
+		LocalDateTime parsedDate;
 		
+		try {
+			parsedDate = LocalDateTime.parse(stringDate, formatter);
+		} catch (Exception e) {
+			System.out.println("Exception thrown while parsing date. Input was \"" + date.toString() + "\".");
+			return new LongWritable(0);
+		}
+		
+		// If the start and end times are bad data, then use 12:00:00am for them. Otherwise, parse them.
+		Pattern timePattern = Pattern.compile("[0-9]{6}");
+		String startTimeString = startTime.toString();
+		String endTimeString = endTime.toString();
+		Matcher startTimeMatch = timePattern.matcher(startTimeString);
+		Matcher endTimeMatch = timePattern.matcher(endTimeString);
+		int startTimeHour = 0;
+		int startTimeMinute = 0;
+		int startTimeSecond = 0;
+		int endTimeHour = 0;
+		int endTimeMinute = 0;
+		int endTimeSecond = 0;
+		
+		if (startTimeMatch.matches()) {
+			startTimeHour = Integer.parseInt(startTimeString.substring(0, 2));
+			startTimeMinute = Integer.parseInt(startTimeString.substring(2, 4));
+			startTimeSecond = Integer.parseInt(startTimeString.substring(4, 6));
+		}
+		
+		if (endTimeMatch.matches()) {
+			endTimeHour = Integer.parseInt(endTimeString.substring(0, 2));
+			endTimeMinute = Integer.parseInt(endTimeString.substring(2, 4));
+			endTimeSecond = Integer.parseInt(endTimeString.substring(4, 6));
+		}
+		
+		// Create two datetime objects - date & start time, date & end time.
+		LocalDateTime startDateTime = parsedDate.plusHours(startTimeHour).plusMinutes(startTimeMinute).plusSeconds(startTimeSecond);
+		LocalDateTime endDateTime = parsedDate.plusHours(endTimeHour).plusMinutes(endTimeMinute).plusSeconds(endTimeSecond);
+		
+		// Calculate the time difference between the start datetime and end datetime.
+		Duration duration = Duration.between(startDateTime, endDateTime);
+		
+		// Return the time difference in minutes.
+		return new LongWritable(duration.toMinutes());
 	}
 }
