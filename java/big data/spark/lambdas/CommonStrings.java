2a6823903ba65a1081f17044be2d06363ec57adf
Christian Kinley <ckinley360@users.noreply.github.com>
2021-10-25 16:44:09 -0700

    Create CommonStrings.java

---
+++ java/big data/spark/lambdas/CommonStrings.java
@@ -0,0 +1,40 @@
+package common;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class CommonStrings {
+    public static List<String> getAlphabetStrings() {
+        String[] strings = {
+            "basket",
+            "helicopter",
+            "xylophone",
+            "jaguar",
+            "nail",
+            "shell",
+            "queen",
+            "drill",
+            "egg",
+            "lace",
+            "flag",
+            "icicle",
+            "cornucopia",
+            "mailbox",
+            "alligator",
+            "yarn",
+            "umbrella",
+            "octopus",
+            "radish",
+            "zebra",
+            "grape",
+            "key",
+            "pail",
+            "tail",
+            "whale",
+            "violin",
+
+        };
+
+        return Arrays.asList(strings);
+    }
+}
