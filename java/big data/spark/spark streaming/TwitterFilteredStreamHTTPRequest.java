8827400fe18c82249863cfc891c5f9a8cf7bf1c6
Christian Kinley <ckinley360@users.noreply.github.com>
2021-11-20 15:30:55 -0800

    Update TwitterFilteredStreamHTTPRequest.java

--- java/big data/spark/spark streaming/TwitterFilteredStreamHTTPRequest.java
+++ java/big data/spark/spark streaming/TwitterFilteredStreamHTTPRequest.java
@@ -3,12 +3,18 @@ package stubs;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
 
 public class TwitterFilteredStreamHTTPRequest {
 
 	private static final String BEARER_TOKEN = "my token";
 	
-	// Retrieves a response from the Twitter Filtered Stream REST API
+	public static void main(String[] args) {
+		System.out.println(getTweets());
+	}
+	
+	// Retrieves a response from the Twitter Filtered Stream API
 	public static String getTweets() {
 		// Create the client
 		HttpClient client = HttpClient.newHttpClient();
@@ -19,5 +25,15 @@ public class TwitterFilteredStreamHTTPRequest {
 			.header("Authorization", "Bearer " + BEARER_TOKEN)
 			.GET()
 			.build();
+		
+		// Send the request and receive the response
+		try {
+			client.sendAsync(request, BodyHandlers.ofString())
+				.thenApply((HttpResponse response) -> String.valueOf(response.statusCode()) + "\n" + response.body() + "\n" + "\n")
+				.thenAccept(responseString -> return responseString);
+			String responseString = String.valueOf(response.statusCode()) + "\n" + response.body();
+		} catch (Exception e) {
+			return "Error: " + e.getMessage();
+		}
 	}
 }
