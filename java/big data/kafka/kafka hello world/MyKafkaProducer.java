53b0cf7d939535656d810a4260888dc3640a93e1
Christian Kinley <ckinley360@users.noreply.github.com>
2021-12-07 16:25:50 -0800

    Update MyKafkaProducer.java

--- java/big data/kafka/kafka hello world/MyKafkaProducer.java
+++ java/big data/kafka/kafka hello world/MyKafkaProducer.java
@@ -1,6 +1,7 @@
 package stubs;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Properties;
@@ -10,9 +11,7 @@ import org.apache.kafka.clients.producer.ProducerRecord;
 
 public class MyKafkaProducer {
 	
-	public static void main(String[] args) {
-		String inputFile = args[0];
-		
+	public void createProducer(String inputFile) throws FileNotFoundException {
 		Properties props = new Properties();
 		props.setProperty("bootstrap.servers", "broker1:9092");
 		props.setProperty("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
@@ -21,26 +20,39 @@ public class MyKafkaProducer {
 		KafkaProducer<String, String> producer = new KafkaProducer<String, String>(props);
 		
 		// Read in data from the text file to create a ProducerRecord, then send it.
-		BufferedReader br = null;
+		BufferedReader br = new BufferedReader(new FileReader(inputFile));
+		String line = null;
+		
 		try {
-			br = new BufferedReader(new FileReader(inputFile));
-			String line = br.readLine();
-			String[] parts = line.split("\t");
-			
-			String key = parts[0];
-			String value = line;
-			
-			ProducerRecord<String, String> record = new ProducerRecord<String, String>("hello_topic", key, value);
-			producer.send(record);
-			producer.close();
+			while ((line = br.readLine()) != null) {
+				String[] parts = line.split("\t");
+				
+				String key = parts[0];
+				String value = line;
+				
+				ProducerRecord<String, String> record = new ProducerRecord<String, String>("hello_topic", key, value);
+				producer.send(record);
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
 			try {
 				br.close();
+				producer.close();
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		} 
+	}
+	
+	public static void main(String[] args) {
+		String inputFile = args[0];
+		MyKafkaProducer producer = new MyKafkaProducer();
+		
+		try {
+			producer.createProducer(inputFile);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
 	}
 }
